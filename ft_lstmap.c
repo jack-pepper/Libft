80666c49856461e94b110c8695d5b2a11637d80b
jack-pepper <70756191+jack-pepper@users.noreply.github.com>
2024-11-13 20:08:23 +0100

    pass all tests

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmalie <mmalie@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/07 10:02:49 by mmalie            #+#    #+#             */
-/*   Updated: 2024/11/08 10:04:57 by mmalie           ###   ########.fr       */
+/*   Created: 2024/11/04 15:34:01 by mmalie            #+#    #+#             */
+/*   Updated: 2024/11/04 15:34:03 by mmalie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
-	t_list	*temp;
 
 	new_lst = NULL;
-	while (lst != NULL)
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
 		if (new_node == NULL)
 		{
-			while (new_lst != NULL)
-			{
-				temp = new_lst->next;
-				del(new_lst->content);
-				free(new_lst);
-				new_lst = temp;
-			}
+			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		if (new_lst == NULL)
-			new_lst = new_node;
-		else
-			temp->next = new_node;
-		temp = new_node;
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
